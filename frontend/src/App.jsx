4fcf9649456f3bc93560809962c19268a7f98a6f
ChristelDeveza <chris.dvz66@gmail.com>
2022-07-17 09:43:55 +0200

    fix nav pb + routes + add logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import Footer from "./components/Footer";
 import SubscriptionPage from "./pages/SubscriptionPage";
 import Navbar from "./components/Navbar";
 import ActivitiesPage from "./pages/ActivitiesPage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="Abonnements" element={<SubscriptionPage />} />
           <Route path="Activites" element={<ActivitiesPage />} />
+          <Route path="APropos" element={<AboutPage />} />
+          <Route path="Contact" element={<ContactPage />} />
         </Routes>
       </Router>
       <Footer />
---
+++ frontend/src/pages/AboutPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>En cours de construction ...</h1>
+    </div>
+  );
+}
+
+export default AboutPage;
