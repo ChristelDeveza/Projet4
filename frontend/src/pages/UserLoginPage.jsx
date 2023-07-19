3908bc142066600277008a9a6c7286f8cfe0fb0f
ChristelDeveza <chris.dvz66@gmail.com>
2023-07-19 15:44:54 +0200

    ajustements

--- frontend/src/pages/UserLoginPage.jsx
+++ frontend/src/pages/UserLoginPage.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Login from "../components/Login";
 import Register from "../components/Register";
+import { UserContext } from "../context/UserContext";
 import "../CSS/Login.css";
 import "../CSS/UserPageLogin.css";
+import UserDashboardPage from "./UserDashboardPage";
 
 function UserLoginPage() {
   const [loginChoice, setLoginChoice] = useState(false);
+  const { isOnline } = useContext(UserContext);
 
   // Interchange between login page and register page
   function showLogin() {
@@ -13,15 +16,24 @@ function UserLoginPage() {
   }
 
   return (
-    <div className="loginpage" id={!loginChoice ? "loginpage" : "registerpage"}>
-      <button
-        className="show-register-button"
-        type="button"
-        onClick={showLogin}
-      >
-        {!loginChoice ? "CREER UN COMPTE" : "ME CONNECTER"}
-      </button>
-      {!loginChoice ? <Login /> : <Register />}
+    <div>
+      {!isOnline ? (
+        <div
+          className="loginpage"
+          id={!loginChoice ? "loginpage" : "registerpage"}
+        >
+          <button
+            className="show-register-button"
+            type="button"
+            onClick={showLogin}
+          >
+            {!loginChoice ? "CREER UN COMPTE" : "ME CONNECTER"}
+          </button>
+          {!loginChoice ? <Login /> : <Register />}
+        </div>
+      ) : (
+        <UserDashboardPage />
+      )}
     </div>
   );
 }
